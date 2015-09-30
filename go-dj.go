2d5801b6bee98cc6ef8cceb78d2bdc8bba6d4f5a
Malcolm Daigle <daiglemalcolm@gmail.com>
2015-09-29 18:52:14 -0700

    Implements basic command line interface.

--- go-dj.go
+++ go-dj.go
@@ -1,1 +1,23 @@
 package main
+
+import(
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	r := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print("Enter command: ")
+		command, err := r.ReadString('\n')
+		command = strings.TrimSpace(command)
+		if err != nil {
+			fmt.Println("Invalid command.")
+		}
+		if command == "end" {
+			break
+		}
+	}
+}
