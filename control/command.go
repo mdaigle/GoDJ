60025791efe8a45c7764f4104bc4650d4ae35fa0
Malcolm Daigle <daiglemalcolm@gmail.com>
2015-10-22 16:02:09 -0700

    Adds more commands.

--- control/command.go
+++ control/command.go
@@ -1,24 +1,31 @@
 package control
 
 import (
-//"spotify"
-	"fmt"
+	//"spotify"
+	format "fmt"
+	"github.com/mdaigle/GoDJ/perform"
 )
 
 func ProcessCommand(command string) bool {
 	switch command {
-	case "p", "play" :
-	//start music streaming
-	case "s", "pause" :
-	//stop music streaming
-	case "b", "browse" :
-	//display music menu
-	case "c", "custom" :
-	//display prompts to specify custom mood
+	case "p" :
+		perform.PlayPause()
+	case "n" :
+		perform.Next()
+	case "b" :
+		perform.Previous()
+	case "i" :
+		perform.IncreaseVolume()
+	case "d" :
+		perform.DecreaseVolume()
+//	case "b", "browse" :
+//	//display music menu
+//	case "c", "custom" :
+//	//display prompts to specify custom mood
 	case "q", "quit" :
 		return true;
 	default:
-		fmt.Print("Invalid Command")
+		format.Print("Invalid Command")
 	}
 	return false;
 
