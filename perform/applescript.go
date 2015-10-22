51d2e4436e602d01c9d9c28f62188ab0845b3905
Malcolm Daigle <daiglemalcolm@gmail.com>
2015-10-22 16:02:42 -0700

    Adds more spotify applescript wrappers.

--- perform/applescript.go
+++ perform/applescript.go
@@ -1,5 +1,27 @@
 package perform
 
 import (
-	//"github.com/everdev/mack"
+	"github.com/everdev/mack"
+	//"strconv"
 )
+
+func PlayPause() {
+	//mack.Say("Playing")
+	mack.Tell("Spotify", "playpause")
+}
+
+func Next() {
+	mack.Tell("Spotify", "next track")
+}
+
+func Previous() {
+	mack.Tell("Spotify", "previous track")
+}
+
+func IncreaseVolume() {
+	mack.Tell("Spotify", "set sound volume to sound volume + 5")
+}
+
+func DecreaseVolume() {
+	mack.Tell("Spotify", "set sound volume to sound volume - 5")
+}
