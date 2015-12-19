9adc9ab36d2201faaeeccececf9d7aa250059124
Malcolm Daigle <daiglemalcolm@gmail.com>
2015-12-19 11:15:47 -0800

    Adds options to command switch.

--- control/command.go
+++ control/command.go
@@ -1,25 +1,28 @@
 package control
 
 import (
-	//"spotify"
+//	"github.com/rapito/go-spotify/spotify"
 	format "fmt"
 	"github.com/mdaigle/GoDJ/perform"
+	"github.com/mdaigle/GoDJ/view"
 )
 
 func ProcessCommand(command string) bool {
 	switch command {
-	case "p" :
-		perform.PlayPause()
-	case "n" :
-		perform.Next()
 	case "b" :
 		perform.Previous()
-	case "i" :
-		perform.IncreaseVolume()
 	case "d" :
 		perform.DecreaseVolume()
+	case "i" :
+		perform.IncreaseVolume()
+	case "h", "help" :
+		view.DisplayMainMenu()
+	case "n" :
+		perform.Next()
+	case "p" :
+		perform.PlayPause()
 //	case "b", "browse" :
-//	//display music menu
+//	display music menu
 //	case "c", "custom" :
 //	//display prompts to specify custom mood
 	case "q", "quit" :
